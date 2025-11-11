e098a49571085ff379867928cc39a5f37762d8fb
Yasmina Soumahoro <m.yasminasoumahoro@gmail.com>
2025-11-11 11:48:56 +0000

    Mise à jour

--- skew_array_PERSO.py
+++ skew_array_PERSO.py
@@ -1,3 +1,4 @@
+import csv
 import matplotlib.pyplot as plt
 
 def gc_variation(base): #The function counts the number of G and C nucleotides.
@@ -20,20 +21,27 @@ def compute_skew(genome): # Calculate the skew of the sequence.
             skew.append(skew[-1])
     return skew
 
-print("compute_skew('AGCGCGTTGCCGATGCGTACG')", compute_skew("AGCGCGTTGCCGATGCGTACGGC"))  #Python will print the text followed by the count value.
+print("compute_skew('CAGTCGCGGATCGATCGTACGCGTACGTGC')", compute_skew("CAGTCGCGGATCGATCGTACGCGTACGTGC"))  #Python will print the text followed by the count value.
 
 # Proceed to display the results in a structured table format.
 def show_gc_variation(genome):
     print("Index | Base | Delta | Skew")
     print("----------------------------")
     skew = [0]
+    results = []
     for i, base in enumerate(genome, start=1):
         d = gc_variation(base) #Call the function here to measure the local effect of each base on the G/C imbalance.
         current = skew[-1] + d #Add the variation of the current base to the last recorded value in the skew list.
         skew.append(current) #Append the new skew value (current) to the skew list for plotting or further analysis.
         print(f"{i:5d} |   {base}   | {d:+d} | {current:d}") #Use an f-string to format and neatly align the calculation progress at each position to visualize the skew dynamics.
+        results.append([i, base, d, current])
 
-show_gc_variation("AGCGCGTTGCCGATGCGTACG")
+# --- Export CSV ---
+    with open("skew_results.csv", "a", newline="", encoding="utf-8") as file: #The result of each test is saved
+        writer = csv.writer(file)
+        writer.writerow(["Index", "Base", "Delta", "Skew"])
+        writer.writerows(results)
+print("\n Résultats enregistrés dans 'skew_results.csv'")
 
 def min_positions(skew): #Return the list of indices (positions) where the skew reaches its minimum value.
     m = min(skew)
@@ -51,3 +59,8 @@ def plot_skew(genome):
     plt.savefig("skew_plot.png", dpi=300, bbox_inches="tight")
     print("Graph saved as skew_plot.png")
     plt.show()
+
+#Compute and export the values.
+show_gc_variation("CAGTCGCGGATCGATCGTACGCGTACGTGC")
+#Generate the plot
+plot_skew("CAGTCGCGGATCGATCGTACGCGTACGTGC")
